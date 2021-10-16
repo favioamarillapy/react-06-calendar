b35045d6eb58b67064fd973a3daaef83b6c63fb9
favioamarillapy <amarillafavio@gmail.com>
2021-10-16 13:12:09 -0300

    Added user register

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -3,11 +3,11 @@ import { fecthNotUsingToken } from '../helpers/fetch'
 import Swal from 'sweetalert2';
 
 
-export const startLogin = (email, password) => {
+export const startLogin = (user) => {
 
     return async (dispatch) => {
 
-        const response = await fecthNotUsingToken('auth/signin', { email, password }, 'POST');
+        const response = await fecthNotUsingToken('auth/signin', user, 'POST');
         const body = await response.json();
 
         if (body.ok) {
@@ -15,11 +15,29 @@ export const startLogin = (email, password) => {
             localStorage.setItem('react-06-calendar-init-date-token', new Date().getTime())
 
             dispatch(startAuth(types.AUTH_SIGNIN, body.data));
-        }else{
+        } else {
             Swal.fire(body.message);
         }
     }
 }
 
+
+export const startRegister = (user) => {
+
+    return async (dispatch) => {
+
+        const response = await fecthNotUsingToken('auth/signup', user, 'POST');
+        const body = await response.json();
+
+        if (body.ok) {
+            localStorage.setItem('react-06-calendar-token', body.data.token)
+            localStorage.setItem('react-06-calendar-init-date-token', new Date().getTime())
+
+            dispatch(startAuth(types.AUTH_SIGNIN, body.data));
+        } else {
+            Swal.fire(body.message);
+        }
+    }
+}
 
 export const startAuth = (type, payload) => ({ type, payload });
--- src/components/auth/AuthScreen.js
+++ src/components/auth/AuthScreen.js
@@ -1,27 +1,64 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useForm } from '../../hook/useForm'
-import { startLogin } from '../../actions/authAction'
+import { startLogin, startRegister } from '../../actions/authAction'
 import './login.css'
+import Swal from 'sweetalert2'
 
 export const AuthScreen = () => {
 
     const dispatch = useDispatch();
 
-    const [formLoginValues, formLoginHandleInputChange, reset] = useForm({ lemail: 'amarillafavio@gmail.com', lpassword: '123456' });
+    const [formLoginValues, formLoginHandleInputChange] = useForm({ lemail: '', lpassword: '' });
     const { lemail, lpassword } = formLoginValues;
 
     const handleLogin = (e) => {
         e.preventDefault();
 
-        dispatch(startLogin(lemail, lpassword));
+        if (lemail == '') {
+            Swal.fire('The fiel email is required');
+            return;
+        }
+        if (lpassword == '') {
+            Swal.fire('The fiel password is required');
+            return;
+        }
+
+        dispatch(startLogin({ email: lemail, password: lpassword }));
+    }
+
+    const [formRegisterValues, formRegisterHandleInputChange] = useForm({ rname: '', remail: '', rpassword: '', rpassword2: '' });
+    const { rname, remail, rpassword, rpassword2 } = formRegisterValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        if (rname == '') {
+            Swal.fire('The fiel name is required');
+            return;
+        }
+        if (remail == '') {
+            Swal.fire('The fiel email is required');
+            return;
+        }
+        if (rpassword == '') {
+            Swal.fire('The fiel password is required');
+            return;
+        }
+
+        if (rpassword != rpassword2) {
+            Swal.fire('Passwords must match');
+            return;
+        }
+
+        dispatch(startRegister({ name: rname, email: remail, password: rpassword }));
     }
 
     return (
         <div className="container login-container">
             <div className="row login-form-container">
                 <div className="col-md-6 login-form-1">
-                    <h3>Ingreso</h3>
+                    <h3>Sign In</h3>
                     <form onSubmit={handleLogin}>
                         <div className="form-group">
                             <input
@@ -54,27 +91,36 @@ export const AuthScreen = () => {
                 </div>
 
                 <div className="col-md-6 login-form-2">
-                    <h3>Registro</h3>
-                    <form>
+                    <h3>Sign Up</h3>
+                    <form onSubmit={handleRegister}>
                         <div className="form-group">
                             <input
                                 type="text"
                                 className="form-control"
-                                placeholder="Nombre"
+                                placeholder="Name"
+                                name="rname"
+                                value={rname}
+                                onChange={formRegisterHandleInputChange}
                             />
                         </div>
                         <div className="form-group">
                             <input
                                 type="email"
                                 className="form-control"
-                                placeholder="Correo"
+                                placeholder="Email"
+                                name="remail"
+                                value={remail}
+                                onChange={formRegisterHandleInputChange}
                             />
                         </div>
                         <div className="form-group">
                             <input
                                 type="password"
                                 className="form-control"
-                                placeholder="Contraseña"
+                                placeholder="Password"
+                                name="rpassword"
+                                value={rpassword}
+                                onChange={formRegisterHandleInputChange}
                             />
                         </div>
 
@@ -82,7 +128,10 @@ export const AuthScreen = () => {
                             <input
                                 type="password"
                                 className="form-control"
-                                placeholder="Repita la contraseña"
+                                placeholder="Repeate Password"
+                                name="rpassword2"
+                                value={rpassword2}
+                                onChange={formRegisterHandleInputChange}
                             />
                         </div>
 
